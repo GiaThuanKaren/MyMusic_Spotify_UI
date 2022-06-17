dfaf8efad77b0bf70c6bc4354906a06cfa2639fa
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-17 15:42:47 +0700

    First Commit 17/6/2022 , Set up server, client

---
+++ Server/Routes/index.js
@@ -0,0 +1,32 @@
+const { ZingMp3 } = require("zingmp3-api-full");
+
+module.exports = function Routes(app) {
+// GET / getHome
+  app.get("/gethome/:page", (req, res) => {
+    console.log(req.params.page);
+    let pageNum = req.params.page ? req.params.page : 1;
+    ZingMp3.getHome(pageNum)
+      .then((data) => {
+        res.json({
+            pageNum,
+            result: data.data
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        res.status(404).json(e)
+      });
+  });
+
+ //GET song 
+
+  app.get("/song/:idSong",(req,res)=>{
+      let idSong = req.params.idSong ? req.params.idSong : "";
+      ZingMp3.getSong(idSong).then((data) => {
+        // console.log(data)
+        res.json(data);
+      }).catch(e=>{
+          res.status(404).json(e)
+      })
+  })
+};
