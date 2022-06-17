cd2953d943ab07357a3ed45920c0b3cfe5989354
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-06-17 15:50:46 +0700

    Add Top 100 Routes, Detail Song 17/6/2022

--- Server/Routes/index.js
+++ Server/Routes/index.js
@@ -1,32 +1,45 @@
 const { ZingMp3 } = require("zingmp3-api-full");
 
 module.exports = function Routes(app) {
-// GET / getHome
+  // GET / getHome
   app.get("/gethome/:page", (req, res) => {
     console.log(req.params.page);
     let pageNum = req.params.page ? req.params.page : 1;
     ZingMp3.getHome(pageNum)
       .then((data) => {
         res.json({
-            pageNum,
-            result: data.data
+          pageNum,
+          result: data.data,
         });
       })
       .catch((e) => {
         console.log(e);
-        res.status(404).json(e)
+        res.status(404).json(e);
       });
   });
 
- //GET song 
+  //GET song
 
-  app.get("/song/:idSong",(req,res)=>{
-      let idSong = req.params.idSong ? req.params.idSong : "";
-      ZingMp3.getSong(idSong).then((data) => {
+  app.get("/song/:idSong", (req, res) => {
+    let idSong = req.params.idSong ? req.params.idSong : "";
+    ZingMp3.getSong(idSong)
+      .then((data) => {
         // console.log(data)
         res.json(data);
-      }).catch(e=>{
-          res.status(404).json(e)
       })
-  })
+      .catch((e) => {
+        res.status(404).json(e);
+      });
+  });
+
+  //GET Top100
+
+  app.get("/top100", (req, res) => {
+    ZingMp3.getTop100().then((data) => {
+      res.status(200).json(data.data)
+    }).catch(e=>{
+      console.log(e)
+      res.status(404).json(e)
+    })
+  });
 };
