7f9ed12251691579da9d06252583acb3ef7da95f
GiaThuanKaren <GiaThuanNguyen213@gmail.com>
2022-08-12 12:24:25 +0700

    Add ListRowAblumSection on Library Component 12/8/2022

--- Client/clientui/src/Components/Card/Card.js
+++ Client/clientui/src/Components/Card/Card.js
@@ -9,24 +9,23 @@ import { CardActionArea, Grid } from "@mui/material";
 //     console.log(item.src)
 // })
 
-export default function CardCatologe() {
+export default function CardCatologe({ Imgpath, tittle, description }) {
   return (
-    <Grid item  lg={2} xl={2} md={2} sm={4} xs={6}>
-      <Card sx={{width:"100%"}}>
+    <Grid item lg={2} xl={2} md={2} sm={4} xs={6}>
+      <Card sx={{ width: "100%" }}>
         <CardActionArea>
           <CardMedia
             component="img"
             height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
+            image={Imgpath}
+            alt="Ablum image"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Lizard
+              {tittle}
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              {description}
             </Typography>
           </CardContent>
         </CardActionArea>
--- Client/clientui/src/Components/Library/Libraray.js
+++ Client/clientui/src/Components/Library/Libraray.js
@@ -1,14 +1,53 @@
-import { Card, Grid } from '@mui/material'
-import React from 'react'
-import CardCatologe from '../Card/Card'
-import style from "./Libraray.module.css"
+import { Card, Grid, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import CardCatologe from "../Card/Card";
+import style from "./Libraray.module.css";
+import axious from "axios";
+const ListrowpayListLibrary = function ({ title, ListPlayListAblum }) {
+  return (
+    <>
+      <div>
+        <Typography component={"p"}>{title}</Typography>
+        {ListPlayListAblum.map((item, idx) => {
+          return <>ds</>;
+        })}
+      </div>
+    </>
+  );
+};
 function Libraray() {
-
-    return (
-       <Grid container spacing={4} className={`${style.Main_Container}`} > 
-            <CardCatologe />
-       </Grid>
-    )
+  const [properties, SetProperties] = useState({
+    ListPlayList: [],
+  });
+  const { ListPlayList } = properties;
+  useEffect(() => {
+    async function FetchApi() {
+      let { data } = await axious.get(`http://localhost:5000/top100`);
+      SetProperties({
+        ...properties,
+        ListPlayList: data,
+      });
+      return data;
+    }
+    FetchApi();
+  }, []);
+  console.log(ListPlayList);
+  return (
+    <Grid container spacing={4}>
+      {/* <CardCatologe /> */}
+      <div className={`${style.Main_Container}`}>
+        {ListPlayList.length != 0 &&
+          ListPlayList.map((item, idx) => {
+            return (
+              <ListrowpayListLibrary
+                title={item.title ? item.title : " Default Tittle"}
+                ListPlayListAblum={item.items}
+              />
+            );
+          })}
+      </div>
+    </Grid>
+  );
 }
 
-export default Libraray
+export default Libraray;
